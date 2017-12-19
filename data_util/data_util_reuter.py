29b5038ac533a0897a028db01c9349c7874f44a4
Aland Lu <luyue0925@foxmail.com>
2017-12-19 18:41:29 +0800

    finish textcnn and reuter

---
+++ data_util/data_util_reuter.py
@@ -0,0 +1,164 @@
+# coding=utf-8
+
+import os
+import re
+import numpy as np
+import tensorflow as tf
+from tflearn.data_utils import VocabularyProcessor, to_categorical
+
+
+# 规范化字符串。SST数据集不需要这么多规则，TREC数据集不需要转小写。
+def normalizeString(string):
+    string = re.sub(r"[^A-Za-z0-9(),!?\'\‘]", " ", string)
+    string = re.sub(r"\'s", " \'s", string)  # It's -> It 's
+    string = re.sub(r"\'ve", " \'ve", string)  # I've -> I 've
+    string = re.sub(r"n\'t", " n\'t", string)  # isn't -> is n't
+    string = re.sub(r"\'re", " \'re", string)  # you're -> you 're
+    string = re.sub(r"\'d", " \'d", string)  # you'd -> you 'd
+    string = re.sub(r"\'ll", " \'ll", string)  # you'll -> you 'll
+    string = re.sub(r",", " , ", string)  # , ->  ,
+    string = re.sub(r"!", " ! ", string)  # ! ->  !
+    string = re.sub(r"\(", " \( ", string)  # ( ->  (
+    string = re.sub(r"\)", " \) ", string)  # ) ->  )
+    string = re.sub(r"\?", " \? ", string)  # ? ->  ?
+    string = re.sub(r"\s{2,}", " ", string)  # 2个以上空白格 -> 保留1个
+    return string.strip().lower()
+
+# 装载数据
+def loadSentenceAndLabel(file_path, label):
+    with open(file_path, 'r') as fin:
+        sentence_list = fin.readlines()
+        normalize_sentence_list = [normalizeString(sentence.strip()) for sentence in sentence_list]
+        label_list = [label for _ in normalize_sentence_list]
+    return normalize_sentence_list, label_list
+
+# 创建词典
+def createVocabulary(sentence_list, vocabulary_path=r"./vocabulary_reuter"):
+    max_sentence_size = max([len(sentence.split(" ")) for sentence in sentence_list])
+    my_vocabulary_processor = VocabularyProcessor(max_sentence_size)
+    print("max sentence size = %d" % max_sentence_size)
+    my_vocabulary_processor.fit(sentence_list)
+    vocabulary_size = len(my_vocabulary_processor.vocabulary_)
+    print("vocabulary size = %d" % vocabulary_size)
+    my_vocabulary_processor.save(vocabulary_path)
+    return my_vocabulary_processor, vocabulary_size
+
+# 句子转数据
+def sentence2Data(sentence_list, vocabulary_path=r"./vocabulary_reuter"):
+    my_vocabulary_processor = VocabularyProcessor.restore(vocabulary_path)
+    data_ndarray = np.array(list(my_vocabulary_processor.transform(sentence_list)))
+    return data_ndarray
+
+# 标签字典
+def createLabelDictionary(label_list):
+    my_label_dictionary = {}
+    label_integer = 0
+    for label in label_list:
+        if label not in my_label_dictionary:
+            my_label_dictionary[label] = label_integer
+            label_integer += 1
+        else:
+            continue
+    return my_label_dictionary
+
+# 标签转数据
+def label2Class(label_list, label_dictionary):
+    label_integer_list = []
+    for label in label_list:
+        label_integer =label_dictionary[label]
+        label_integer_list.append(label_integer)
+    return np.array(list(label_integer_list))
+
+# 打乱顺序
+def shuffleData(data_ndarray, class_ndarray):
+    np.random.seed(10)
+    shuffle_indices = np.random.permutation(np.arange(len(class_ndarray)))
+    shuffle_data_ndarray = data_ndarray[shuffle_indices]
+    shuffle_class_ndarray = class_ndarray[shuffle_indices]
+    return shuffle_data_ndarray, shuffle_class_ndarray
+
+# 分成训练集和验证集
+def splitData(data_ndarry, class_ndarry, valid_percentage):
+    valid_index = -1 * int(valid_percentage * float(len(data_ndarry)))
+    train_x_ndarray, valid_x_ndarray = data_ndarry[:valid_index], data_ndarry[valid_index:]
+    train_y_ndarray, valid_y_ndarray = class_ndarry[:valid_index], class_ndarry[valid_index:]
+    return train_x_ndarray, train_y_ndarray, valid_x_ndarray, valid_y_ndarray
+
+# 生成batch
+def generateBatch(data_list, batch_size, epoch_count, is_shuffle=True):
+    data_ndarray = np.array(data_list)
+    data_size = len(data_list)
+    batch_count_per_epoch = int((len(data_ndarray)-1)/batch_size) + 1
+    for epoch in range(epoch_count):
+        # 是否全排列，得到一个新的列表
+        if is_shuffle:
+            shuffle_indices = np.random.permutation(np.arange(data_size))
+            shuffled_data_list = data_ndarray[shuffle_indices]
+        else:
+            shuffled_data_list = data_ndarray
+        for batch_index in range(batch_count_per_epoch):
+            start_index = batch_index * batch_size
+            end_index = min((batch_index + 1) * batch_size, data_size)
+            yield shuffled_data_list[start_index: end_index]
+
+
+if __name__ == "__main__":
+    print("===")
+    # 输出路径
+    output_file_folder = os.path.abspath(os.path.join(os.path.curdir, "run"))
+    # 1)load data
+    sentence_list_0, label_list_0 = loadSentenceAndLabel("../data/reuter/r8-train/acq.txt", "acq")
+    sentence_list_1, label_list_1 = loadSentenceAndLabel("../data/reuter/r8-train/crude.txt", "crude")
+    sentence_list_2, label_list_2 = loadSentenceAndLabel("../data/reuter/r8-train/earn.txt", "earn")
+    sentence_list_3, label_list_3 = loadSentenceAndLabel("../data/reuter/r8-train/grain.txt", "grain")
+    sentence_list_4, label_list_4 = loadSentenceAndLabel("../data/reuter/r8-train/interest.txt", "interest")
+    sentence_list_5, label_list_5 = loadSentenceAndLabel("../data/reuter/r8-train/money-fx.txt", "money-fx")
+    sentence_list_6, label_list_6 = loadSentenceAndLabel("../data/reuter/r8-train/ship.txt", "ship")
+    sentence_list_7, label_list_7 = loadSentenceAndLabel("../data/reuter/r8-train/trade.txt", "trade")
+    all_sentence_list = sentence_list_0 + sentence_list_1 + sentence_list_2 + sentence_list_3 + \
+                        sentence_list_4 + sentence_list_5 + sentence_list_6 + sentence_list_7
+    all_label_list = label_list_0 + label_list_1 + label_list_2 + label_list_3 + \
+                     label_list_4 + label_list_5 + label_list_6 + label_list_7
+    print("sentence count = %d" % len(all_sentence_list))
+    # 2)build vocabulary
+    vocabulary_processor = createVocabulary(all_sentence_list)
+    # 3)transform from sentence to data
+    data_ndarray_0 = sentence2Data(sentence_list_0)
+    data_ndarray_1 = sentence2Data(sentence_list_1)
+    data_ndarray_2 = sentence2Data(sentence_list_2)
+    data_ndarray_3 = sentence2Data(sentence_list_3)
+    data_ndarray_4 = sentence2Data(sentence_list_4)
+    data_ndarray_5 = sentence2Data(sentence_list_5)
+    data_ndarray_6 = sentence2Data(sentence_list_6)
+    data_ndarray_7 = sentence2Data(sentence_list_7)
+    all_data_ndarray = np.concatenate((data_ndarray_0, data_ndarray_1, data_ndarray_2, data_ndarray_3,
+                                       data_ndarray_4, data_ndarray_5, data_ndarray_6, data_ndarray_7), 0)
+    # 4)create label dictionary
+    label_dictionary = createLabelDictionary(all_label_list)
+    print(label_dictionary)
+    # 5)transform from label to data
+    class_ndarray_0 = to_categorical(label2Class(label_list_0, label_dictionary), 8)
+    print(class_ndarray_0[0])
+    class_ndarray_1 = to_categorical(label2Class(label_list_1, label_dictionary), 8)
+    print(class_ndarray_1[0])
+    class_ndarray_2 = to_categorical(label2Class(label_list_2, label_dictionary), 8)
+    print(class_ndarray_2[0])
+    class_ndarray_3 = to_categorical(label2Class(label_list_3, label_dictionary), 8)
+    print(class_ndarray_3[0])
+    class_ndarray_4 = to_categorical(label2Class(label_list_4, label_dictionary), 8)
+    print(class_ndarray_4[0])
+    class_ndarray_5 = to_categorical(label2Class(label_list_5, label_dictionary), 8)
+    print(class_ndarray_5[0])
+    class_ndarray_6 = to_categorical(label2Class(label_list_6, label_dictionary), 8)
+    print(class_ndarray_6[0])
+    class_ndarray_7 = to_categorical(label2Class(label_list_7, label_dictionary), 8)
+    print(class_ndarray_7[0])
+    all_class_ndarray = np.concatenate((class_ndarray_0, class_ndarray_1, class_ndarray_2, class_ndarray_3,
+                                        class_ndarray_4, class_ndarray_5, class_ndarray_6, class_ndarray_7), 0)
+    # 6)shuffle data
+    del all_sentence_list, all_label_list
+    shuffle_data_ndarray, shuffle_class_ndarray = shuffleData(all_data_ndarray, all_class_ndarray)
+    # 7)split
+    train_x_ndarry, train_y_ndarry, valid_x_ndarry, valid_y_ndarry = \
+        splitData(shuffle_data_ndarray, shuffle_class_ndarray, 0.1)
+
--- text_cnn/text_cnn_model.py
+++ text_cnn/text_cnn_model.py
@@ -1,1 +1,68 @@
 # coding=utf-8
+
+import tensorflow as tf
+
+class TextCNN(object):
+    # 构造函数
+    def __init__(self,
+                 sequence_size, label_count,
+                 vocabulary_size, embedded_size,
+                 filter_size_list, filter_feature_count,
+                 l2_regularization_lambda=0.0):
+        # 输入层
+        self.input_x = tf.placeholder(tf.int32, [None, sequence_size], name="input_x")
+        self.input_y = tf.placeholder(tf.float32, [None, label_count], name="input_y")
+        # 嵌入层
+        with tf.device("/:cpu:0"), tf.name_scope("embedded_layer"):
+            self.embedded_w = tf.Variable(tf.random_uniform([vocabulary_size, embedded_size], -1.0, 1.0),
+                                          name="embedded_w")
+            embedded_out = tf.nn.embedding_lookup(self.embedded_w, self.input_x, name="embedded_out")  # 查找词向量
+            embedded_out_expanded = tf.expand_dims(embedded_out, -1, name="embedded_out_expanded")  # 3维填充为4维
+        # 卷积池化层
+        pooled_out_list = []
+        for i, filter_size in enumerate(filter_size_list):
+            with tf.name_scope("convolutional_maxpool_layer_%s" % filter_size):
+                # 卷积
+                filter_shape = [filter_size, embedded_size, 1, filter_feature_count]
+                convolutional_w = tf.Variable(tf.truncated_normal(filter_shape, 0.0, -0.1),
+                                              name="convolutional_w")
+                convolutional_b = tf.Variable(tf.constant(0.1, shape=[filter_feature_count]),
+                                              name="convolutional_b")
+                convolutional_out = tf.nn.conv2d(embedded_out_expanded, convolutional_w,
+                                                       strides=[1, 1, 1, 1], padding="VALID",
+                                                       name="convolutional_out")
+                # 激活
+                convolutional_h = tf.nn.relu(tf.nn.bias_add(convolutional_out, convolutional_b),
+                                             name="convolutional_relu")
+                # 池化
+                pooled_out = tf.nn.max_pool(convolutional_h, ksize=[1, sequence_size-filter_size+1, 1, 1],
+                                        strides=[1, 1, 1, 1], padding="VALID", name="pooled_out")
+                pooled_out_list.append(pooled_out)
+        filter_feature_total_count = filter_feature_count * len(filter_size_list)
+        filter_feature_concat = tf.concat(pooled_out_list, 3)  # 串联池化结果
+        filter_feature_flat = tf.reshape(filter_feature_concat, [-1, filter_feature_total_count])  # 展平
+        # dropout
+        with tf.name_scope("dropout"):
+            self.dropout_keep_probability = tf.placeholder(tf.float32, name="dropout_keep_probability")
+            dropout_out = tf.nn.dropout(filter_feature_flat, self.dropout_keep_probability, name="dropout_out")
+        # 输出层
+        with tf.name_scope("output_layer"):
+            output_w = tf.get_variable("output_w", shape=[filter_feature_total_count, label_count],
+                                       initializer=tf.contrib.layers.xavier_initializer())
+            output_b = tf.Variable(tf.constant(0.1, shape=[label_count]), name="output_b")
+            l2_loss = tf.constant(0.0)
+            l2_loss = l2_loss + tf.nn.l2_loss(output_w) + tf.nn.l2_loss(output_b)
+            self.output_out = tf.nn.xw_plus_b(dropout_out, output_w, output_b, name="output_out")
+            output_label = tf.argmax(self.output_out, 1, name="output_label")
+        # 损失函数
+        with tf.name_scope("loss_function"):
+            loss = tf.nn.softmax_cross_entropy_with_logits(labels=self.input_y, logits=self.output_out)
+            self.loss_function = tf.reduce_mean(loss) + l2_regularization_lambda * l2_loss
+        # 准确率
+        with tf.name_scope("accuracy_result"):
+            correct_prediction = tf.equal(output_label, tf.argmax(self.input_y, 1))
+            self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"), name="accuracy_result")
+
+
+
+
